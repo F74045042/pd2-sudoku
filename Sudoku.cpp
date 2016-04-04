4303c8431a4949eab29611988d9ad48d7a3f3376
F74045042 <sunshangjih0819@gmail.com>
2016-04-04 23:27:14 +0800

    modified files

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -6,13 +6,15 @@ using namespace std;
 
 void Sudoku::giveQuestion(){
 	int i;
-/*	ifstream sudokuQ("sudokuQ.in",ios::in);
-	for(i=0; i<sudokuSize; i++){
+	/*	ifstream sudokuQ("sudokuQ.in",ios::in);
+		for(i=0; i<sudokuSize; i++){
 		sudokuQ>>board[i];
-	}*/
-	int board[sudokuSize]={8,0,0,0,0,0,0,0,0,0,0,3,6,0,0,0,0,0,0,7,0,0,9,0,2,0,0,0,5,0,0,0,7,0,0,0,0,0,0,0,4,5,7,0,0,0,0,0,1,0,0,0,3,0,0,0,1,0,0,0,0,6,8,0,0,8,5,0,0,0,1,0,0,9,0,0,0,0,4,0,0};
-	transform();
-	printOut();
+		}*/
+	int boardTest[sudokuSize]={8,0,0,0,0,0,0,0,0,0,0,3,6,0,0,0,0,0,0,7,0,0,9,0,2,0,0,0,5,0,0,0,7,0,0,0,0,0,0,0,4,5,7,0,0,0,0,0,1,0,0,0,3,0,0,0,1,0,0,0,0,6,8,0,0,8,5,0,0,0,1,0,0,9,0,0,0,0,4,0,0};
+	//transform();
+	for(i=0;i<sudokuSize;i++){
+		cout<<boardTest[i]<<((i+1)%9==0?'\n':' ');
+	}
 }
 
 void Sudoku::readIn(){
